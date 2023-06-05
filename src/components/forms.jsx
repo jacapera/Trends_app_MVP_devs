d8136b32898d87e72be571274b822ed53133a449
Hernán M <hernan.magallanes.94@gmail.com>
2023-06-04 23:56:44 -0300

    add forms and discovery

---
+++ src/components/forms.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export function StudenForm() {
+	return (
+		<form>
+			<label>
+				Enter your name:
+				<input type="text" />
+			</label>
+		</form>
+	);
+}
