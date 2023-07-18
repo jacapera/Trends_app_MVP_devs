b4cf1d43c0fe46830904da586072c539564b91a2
EBosch14 <enzo.jbosch@gmail.com>
2023-07-18 16:01:45 -0300

    feat(src/components): :sparkles: add multiple texts input
    add different courses in the "Courses Completed" field

--- src/components/customTextInput/index.jsx
+++ src/components/customTextInput/index.jsx
@@ -1,6 +1,8 @@
 import { TextInput } from "@tremor/react";
+import { useEffect, useState } from "react";
+import { ArrowCircleRightIcon } from "@heroicons/react/solid";
 
-export default function CustomTextInput({
+export function CustomTextInput({
   label,
   value,
   type,
@@ -10,7 +12,7 @@ export default function CustomTextInput({
   handleKeyDown,
   error,
   defaultChecked,
-  id
+  id,
 }) {
   return (
     <div>
@@ -27,6 +29,61 @@ export default function CustomTextInput({
         onKeyDown={handleKeyDown}
         defaultChecked={defaultChecked}
       />
+    </div>
+  );
+}
+
+export function CustomMultiTextInput({
+  label,
+  values,
+  type,
+  name,
+  placeholder,
+  //handleInput,
+  // handleKeyDown,
+  error,
+  id,
+}) {
+  const [value, setValue] = useState("");
+  const [options, setOptions] = useState(values);
+
+  const handleInput = (event) => {
+    setValue(event.target.value);
+    if (value && event.keyCode === 13) {
+      setOptions((prevState) => [...new Set([...prevState, value])]);
+      setValue("");
+    }
+  };
+
+  useEffect(() => {
+    console.log(options);
+  }, [options]);
+
+  return (
+    <div>
+      {label && <label htmlFor={name}>{label}</label>}
+      <TextInput
+        value={value}
+        type={type}
+        name={name}
+        error={!!error}
+        errorMessage={error}
+        id={id ? id : name}
+        placeholder={placeholder}
+        onChange={handleInput}
+        onKeyDown={handleInput}
+        icon={ArrowCircleRightIcon}
+      />
+      <div>
+        {options.map((el, i) => (
+          <span
+            key={`${el} ${i}`}
+            onClick={() => setOptions(options.filter((opt) => opt !== el))}
+          >
+            {el}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
