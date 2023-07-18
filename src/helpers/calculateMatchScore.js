419531e40311ae00d3d2b9cc83ae3802dba32258
JeremiasVillane <vil.jeremias@gmail.com>
2023-07-18 18:43:23 -0300

    refactor,add(helpers/calculateMatchScore): using sets, adding academic types match

--- src/helpers/calculateMatchScore.js
+++ src/helpers/calculateMatchScore.js
@@ -1,12 +1,8 @@
 function calculateMatchScore(profile1, profile2) {
   // Se define el "peso" que tiene cada campo del perfil
   // 5 = Alto, 3 = Medio, 1 = Bajo
-  // Podría ser mejor refinar aún más el puntaje de cada campo
-  // Por ejemplo: Alto = de 5 a 3, etc
   const weights = {
     "profile.support": 5,
-    // "academic.type": 5,  // Falta desarrollar la lógica
-                            // En este caso no habrá equivalencias 1:1
     "academic.area": 5,
     "info.career": 5,
     "info.interests": 5,
@@ -20,6 +16,18 @@ function calculateMatchScore(profile1, profile2) {
     "academic.institution": 1,
     "academic.graduation": 1,
   };
+
+  // Diccionario para matchear los tipos académicos
+  const academicTypes = {
+    "Secundaria": "Sin Experiencia",
+    "Universitario Junior": "Junior",
+    "Universitario Intermedio": "Middle",
+    "Universitario Avanzado": "Senior",
+  };
+
+  // Desestructuración de los tipos académicos de los perfiles
+  const { academic: { type: type1 } } = profile1;
+  const { academic: { type: type2 } } = profile2;
 
   // El "puntaje" de matcheo
   let score = 0;
@@ -36,16 +44,18 @@ function calculateMatchScore(profile1, profile2) {
         Array.isArray(profile[outerKey]?.[innerKey])
       )
     ) {
-      const commonItems = profile1[outerKey][innerKey].filter((item) =>
-        profile2[outerKey][innerKey].includes(item)
+      
+      // Se convierte cada array en un set
+      // Los sets son más eficientes
+      const field1 = new Set(profile1[outerKey][innerKey]);
+      const field2 = new Set(profile2[outerKey][innerKey]);
+      const commonData = new Set(
+        [...field1].filter((data) => field2.has(data))
       );
 
-      // Si tiene al menos un item en común se suma al score
-      // (Se podría especificar cuántos puntos sumar
-      // dependiendo de la cantidad de coincidencias)
-      if (commonItems.length > 0) {
-        score += weights[key];
-      }
+      // Se suma según los datos en común
+      score += weights[key] * commonData.size;
+
       // Las demás propiedades van a ser un solo value (una vez elegido)
     } else if (
       profile1[outerKey] &&
@@ -57,5 +67,10 @@ function calculateMatchScore(profile1, profile2) {
     }
   }
 
+  // Se usa el diccionario para matchear el tipo académico
+  if (academicTypes[type2] === type1 || academicTypes[type1] === type2) {
+    score += 5;
+  }
+  
   return score;
 }
