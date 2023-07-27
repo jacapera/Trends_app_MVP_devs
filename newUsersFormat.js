8f0c9ea277f7b03d2798a5a028528942fd383aa1
EBosch14 <enzo.jbosch@gmail.com>
2023-07-27 20:55:17 -0300

    refactor(api/): :truck: change file name

--- newUsersFormat.js
+++ newUsersFormat.js
@@ -1,35 +1,3 @@
-const user1 = {
-  type: "Student",
-  email: "juan.perez@example.com",
-  username: "juanperez",
-  password: "contraseña123",
-  name: "Juan Perez",
-  bio: "",
-  age: "23",
-  image: "",
-  city: "Buenos Aires",
-  country: "Argentina",
-  support: true,
-  formation: "Universitario Avanzado",
-  institution: "Universidad Nacional de Buenos Aires",
-  level: "En curso",
-  area: ["Ingeniería Informática"],
-  graduation: "2023",
-  company_name: "", //Only for professionals
-  position: "", //Only for professionals
-  career: ["Desarrollo de Software"],
-  skills: ["Programación en Python", "Desarrollo web", "Bases de datos"],
-  goals: ["Elegir una carrera", "Encontrar una pasantía o trabajo"],
-  interests: ["Inteligencia Artificial", "Desarrollo de aplicaciones móviles"],
-  problematic: [
-    "Falta de información del mercado laboral",
-    "Falta de guía profesional",
-  ],
-  languages: ["Español", "Inglés"],
-  availability: "Full-time",
-  contract: "Remoto",
-};
-
 const user2 = {
   type: "Professional",
   email: "juan.perez@example.com",
@@ -63,4 +31,52 @@ const user2 = {
   info_languages: ["Español", "Inglés"],
   info_availability: "Full-time",
   info_contract: "Remoto",
+};
+
+const company1 = {
+  type: "Company",
+  data: {
+    company_name: "Coca Cola",
+    cuit: "27303255418",
+    website: "https://cocacola.com.ar/",
+    bio: "",
+    image: "",
+    username: "juanperez",
+    email: "coca.cola@example.com",
+    password: "password",
+    city: "Buenos Aires",
+    country: "Argentina",
+  },
+};
+
+const company2 = {
+  type: "Company",
+  data: {
+    company_name: "Pixar",
+    cuit: "35603255418",
+    website: "https://pixar.com.ar/",
+    bio: "",
+    image: "",
+    username: "stevejobs",
+    email: "pixar@example.com",
+    password: "pass123",
+    city: "Buenos Aires",
+    country: "Argentina",
+  },
+};
+
+const company3 = {
+  type: "Company",
+  data: {
+    company_name: "Trends SA",
+    cuit: "27303211111",
+    website: "https://trends.com.ar/",
+    bio: "",
+    image: "",
+    username: "markzuk",
+    email: "trends@mail.com",
+    password: "123456",
+    city: "Buenos Aires",
+    country: "Argentina",
+  },
 };
