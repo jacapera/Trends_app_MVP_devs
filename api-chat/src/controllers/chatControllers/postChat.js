94f5eb9697a5fff82d0721b1586630af756e9bda
jacapera <capera125@gmail.com>
2023-07-27 17:19:33 -0500

    api-chats  remove
    se integra el socket.io en api, se quita api-chat para trabajar solamente en api

--- api-chat/src/controllers/chatControllers/postChat.js
+++
@@ -1,8 +0,0 @@
-const { Chat, Message, User } = require('../../db');
-
-const postChat = async (user_id) => {
-  const newChat = await Chat.create();
-  return newChat;
-}
-
-module.exports = postChat;
