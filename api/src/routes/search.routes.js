0dab77be8fe44e7c362ff796ab67c9a8866b7345
jacapera <capera125@gmail.com>
2023-07-27 18:29:16 -0500

    se elimina backend para evitar conflicto con los otro PR

--- api/src/routes/search.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { searchUserById, searchUsers } = require("../handlers/search.handlers");
-const searchRoutes = Router();
-
-searchRoutes.get("/user/:id", searchUserById);
-searchRoutes.get("/users", searchUsers);
-
-module.exports = searchRoutes;
