059af7918aa186abb23882bd8756bccdbd532211
Hernán M <hernan.magallanes.94@gmail.com>
2023-07-28 17:24:55 -0300

    Revert "hand made merge files modified: register controller , db.js , auth.handlers, createUse, auto.routes"
    This reverts commit c91ed317f450fc323d8950b0da3edb4c78b2a6e6.

--- api/src/routes/auth.routes.js
+++ api/src/routes/auth.routes.js
@@ -1,16 +1,10 @@
-const { Router } = require("express");
-const {
-	register,
-	login,
-	logout,
-	profile,
-} = require("../handlers/auth.handlers");
+const {Router} = require("express")
+const { register, login, logout } = require("../handlers/auth.handlers")
 
-const authRoutes = Router();
+const authRoutes = Router()
 
-authRoutes.post("/register", register);
-authRoutes.post("/login", login);
-authRoutes.post("/logout", logout);
-authRoutes.get("/auth/profile", profile);
+authRoutes.post("/register", register)
+authRoutes.post("/login", login)
+authRoutes.post("/logout", logout)
 
-module.exports = authRoutes;
+module.exports = authRoutes
