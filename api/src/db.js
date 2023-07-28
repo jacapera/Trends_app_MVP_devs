a91afc8dc9f10d528b9ba0670bfcd62a9e0d0e67
EBosch14 <enzo.jbosch@gmail.com>
2023-07-28 10:15:35 -0300

    Revert "hand made merge files modified: register controller , db.js , auth.handlers, createUse, auto.routes"
    This reverts commit c91ed317f450fc323d8950b0da3edb4c78b2a6e6.

--- api/src/db.js
+++ api/src/db.js
@@ -3,19 +3,19 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const {
-	DB_USER,
-	DB_PASSWORD,
-	DB_HOST,
-	DB_PORT,
-	DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
 } = require("../config");
 
 const sequelize = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-	{
-		logging: false,
-		native: false,
-	}
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  {
+    logging: false,
+    native: false,
+  }
 );
 
 const basename = path.basename(__filename);
@@ -24,131 +24,29 @@ const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, "/models"))
-	.filter(
-		(file) =>
-			file.indexOf(".") !== 0 &&
-			file !== basename &&
-			file.slice(-3) === ".js"
-	)
-	.forEach((file) => {
-		modelDefiners.push(require(path.join(__dirname, "/models", file)));
-	});
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  });
 
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [
-	entry[0][0].toUpperCase() + entry[0].slice(1),
-	entry[1],
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
 ]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-//const { User } = sequelize.models;
-
-const {
-	//User,
-	//Message,
-	Profile,
-	Academic,
-	Info,
-	InfoProfessional,
-	Student,
-	Professional,
-} = sequelize.models;
-
-//Message.belongsTo(User, { foreignKey: "user_id", allowNull: false });
-
-// ESTUDIANTES
-Student.belongsTo(Profile, {
-	foreignKey: {
-		name: "studentProfile",
-		allowNull: false,
-	},
-});
-Profile.hasOne(Student, {
-	foreignKey: {
-		name: "studentProfile",
-		allowNull: false,
-	},
-});
-
-Student.belongsTo(Academic, {
-	foreignKey: {
-		name: "studentAcademic",
-		allowNull: false,
-	},
-});
-Academic.hasOne(Student, {
-	foreignKey: {
-		name: "studentAcademic",
-		allowNull: false,
-	},
-});
-
-Student.belongsTo(Info, {
-	foreignKey: {
-		name: "studentInfo",
-		allowNull: false,
-	},
-});
-Info.hasOne(Student, {
-	foreignKey: {
-		name: "studentInfo",
-		allowNull: false,
-	},
-});
-
-// PROFESIONALES
-Professional.belongsTo(Profile, {
-	foreignKey: {
-		name: "professionalProfile",
-		allowNull: false,
-	},
-});
-Profile.hasOne(Professional, {
-	foreignKey: {
-		name: "professionalProfile",
-		allowNull: false,
-	},
-});
-
-Professional.belongsTo(Academic, {
-	foreignKey: {
-		name: "professionalAcademic",
-		allowNull: false,
-	},
-});
-Academic.hasOne(Professional, {
-	foreignKey: {
-		name: "professionalAcademic",
-		allowNull: false,
-	},
-});
-
-// Professional.belongsTo(Info, {
-//   foreignKey: {
-//     name: "professionalInfo",
-//     allowNull: false,
-//   },
-// });
-Info.hasOne(Professional, {
-	foreignKey: {
-		name: "professionalInfo",
-		allowNull: false,
-	},
-});
-
-Professional.belongsTo(Info, { foreignKey: "infoId", as: "info" });
-Professional.belongsTo(InfoProfessional, {
-	foreignKey: "infoId",
-	as: "infoProfessional",
-});
+const { User } = sequelize.models;
 
 module.exports = {
-
-	...sequelize.models,
-	conn: sequelize,
+  ...sequelize.models,
+  conn: sequelize,
 };
--- api/src/helpers/createUser.js
+++ api/src/helpers/createUser.js
@@ -1,16 +1,5 @@
 const { User, Company, conn } = require("../db");
 
-const {
-	Profile,
-	Academic,
-	Info,
-	Student,
-	InfoProfessional,
-	Professional,
-	conn,
-} = require("../db");
-
-/*
 const createNewUser = async (userData) => {
   try {
     // console.log(userData);
@@ -20,54 +9,20 @@ const createNewUser = async (userData) => {
     throw new Error(`User could not be created. ${error}`);
   }
 };
-*/
-
-const createNewStudent = async (userData, hashedPassword) => {
-	const { profile, academic, info } = userData;
-	const transaction = await conn.transaction();
-	// console.log(hashedPassword);
-	// console.log({ ...profile, ...academic, ...info });
-	try {
-		const studentProfile = await Profile.create(
-			{ ...profile, password: hashedPassword },
-			{ transaction }
-		);
-		const studentAcademic = await Academic.create(academic, {
-			transaction,
-		});
-		const studentInfo = await Info.create(info, { transaction });
-		const createdStudent = await Student.create(
-			{
-				studentProfile: studentProfile.id,
-				studentAcademic: studentAcademic.id,
-				studentInfo: studentInfo.id,
-			},
-			{ transaction }
-		);
-		await transaction.commit();
-		return createdStudent;
-	} catch (error) {
-		await transaction.rollback();
-		throw new Error(`Student could not be created. ${error}`);
-	}
-};
-
-// const createNewProfessional = async (userData, hashedPassword) => {};
 
 const createNewCompany = async (companyData) => {
-	const { data } = companyData;
-	try {
-		// console.log(data);
-		const newCompany = await Company.create(data);
-		console.log(newCompany);
-		return newCompany;
-	} catch (error) {
-		throw new Error(`Company could not be created. ${error}`);
-	}
+  const { data } = companyData;
+  try {
+    // console.log(data);
+    const newCompany = await Company.create(data);
+    console.log(newCompany);
+    return newCompany;
+  } catch (error) {
+    throw new Error(`Company could not be created. ${error}`);
+  }
 };
 
 module.exports = {
-	createNewCompany,
-	createNewProfessional,
-	createNewStudent,
+  createNewCompany,
+  createNewUser,
 };
