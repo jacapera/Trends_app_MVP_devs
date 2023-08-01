838e5567888b6ef57c47dc15bc6a8ad49dacdd1b
GDavidDias <guillermo.david.dias@gmail.com>
2023-08-01 18:14:52 -0300

    modificacion de vista para adaptar el back feedCompany, ProfileCompany, CandidatesCompany, CompanyJobs

--- api/src/app.js
+++ api/src/app.js
@@ -31,8 +31,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/user", authenticateUser, userRoutes);
-app.use("/api/v1/search", authenticateUser, searchRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/search", searchRoutes);
 
 // --- solo para pruebas ---
 app.use("/userTest", userTestRoutes);
