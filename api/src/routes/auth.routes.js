c01d082ab72f001be9a71c21c8a70ebf427bbf2b
EBosch14 <enzo.jbosch@gmail.com>
2023-08-02 01:37:29 -0300

    feat(api/): :sparkles: add validation info middleware for register and login

--- api/src/routes/auth.routes.js
+++ api/src/routes/auth.routes.js
@@ -1,10 +1,15 @@
-const {Router} = require("express")
-const { register, login, logout } = require("../handlers/auth.handlers")
+const { Router } = require("express");
+const { register, login, logout } = require("../handlers/auth.handlers");
+const validateSchema = require("../middlewares/validateInfo");
+const {
+  registerUserSchema,
+  loginUserSchema,
+} = require("../schemas/auth.schema");
 
-const authRoutes = Router()
+const authRoutes = Router();
 
-authRoutes.post("/register", register)
-authRoutes.post("/login", login)
-authRoutes.post("/logout", logout)
+authRoutes.post("/register", validateSchema(registerUserSchema), register);
+authRoutes.post("/login", validateSchema(loginUserSchema), login);
+authRoutes.post("/logout", logout);
 
-module.exports = authRoutes
+module.exports = authRoutes;
