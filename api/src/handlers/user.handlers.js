5f0cfffdf89e7515ed51bef0c7fa8dd0a81939fd
EBosch14 <enzo.jbosch@gmail.com>
2023-08-02 04:52:53 -0300

    feat(api/): :zap: change how user profile data is returned and add functionality to change password

--- api/src/handlers/user.handlers.js
+++ api/src/handlers/user.handlers.js
@@ -1,11 +1,28 @@
-const { getUserFeed } = require("../controllers/user.controller");
+const {
+  getUserFeed,
+  getUserProfile,
+  changeUserPassword,
+} = require("../controllers/user.controller");
 
 const profile = async (req, res) => {
   const { user } = req;
-  // console.log(user);
   try {
     console.log("user profile, route protected.");
-    res.status(200).json(user);
+    const foundedUser = await getUserProfile(user);
+    if (!foundedUser) throw new Error("User not found.");
+    res.status(200).json(foundedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const updatePassword = async (req, res) => {
+  const { id } = req.user;
+  const newPassword = req.body.password;
+  try {
+    const result = await changeUserPassword(id, newPassword);
+    if (!result) throw new Error("Could not update password.");
+    res.status(200).json("Password updated successfully.");
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -38,4 +55,4 @@ const feed = async (req, res) => {
   }
 };
 
-module.exports = { profile, feed };
+module.exports = { profile, feed, updatePassword };
