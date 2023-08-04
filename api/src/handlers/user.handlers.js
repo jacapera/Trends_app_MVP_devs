5e52f707382910be939c148bf8abce56337c98ed
AGomezCuervo <107006627+AGomezCuervo@users.noreply.github.com>
2023-08-04 14:11:24 -0500

    fixing problems with global styles

--- api/src/handlers/user.handlers.js
+++ api/src/handlers/user.handlers.js
@@ -47,21 +47,7 @@ const editProfile = async (req, res) => {
   }
 };
 
-const removeProfile = async (req, res) => {
-  const { id } = req.params;
 
-  try {
-    const removedProfile = await deleteUserProfile(id);
-
-    if (removedProfile === 0) {
-      return res.status(400).json({ error: "User not found" });
-    }
-
-    res.status(200).json({ message: "Profile successfully removed" });
-  } catch (error) {
-    res.status(500).json({ error: "The profile couldn't be deleted" });
-  }
-};
 
 const removeProfile = async (req, res) => {
   const { id } = req.params;
--- api/src/middlewares/validateCompany.js
+++ api/src/middlewares/validateCompany.js
@@ -1,13 +1,7 @@
 const validateCompany = async (req, res, next) => {
-<<<<<<< HEAD
-  const { type: userType} = req.user
-
-  if (userType !== "company") {
-=======
   const { type: userType } = req.user;
 
   if (userType.toLowerCase() !== "company") {
->>>>>>> 036c58092a065ae28621c9e8ad95ad3dabd69405
     return res.status(400).json({ error: "Invalid or not authorized user" });
   }
 
