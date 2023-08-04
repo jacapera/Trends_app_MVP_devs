49b8ea41691488486d15ce4f0a4f439dcd055a8f
EBosch14 <enzo.jbosch@gmail.com>
2023-08-04 05:35:06 -0300

    feat(api/): :sparkles: create default admin account

--- api/config.js
+++ api/config.js
@@ -3,6 +3,8 @@ const DEFAULT_IMG =
   "https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80";
 
 const {
+  ADMIN_USERNAME,
+  ADMIN_PASSWORD,
   NODE_ENV,
   SV_HOST,
   SV_PORT,
@@ -15,6 +17,8 @@ const {
 } = process.env;
 
 module.exports = {
+  ADMIN_USERNAME,
+  ADMIN_PASSWORD,
   DEFAULT_IMG,
   NODE_ENV,
   SV_HOST,
