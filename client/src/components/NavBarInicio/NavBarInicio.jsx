0de696163874c3d6401d6724487ce74fc2279642
AGomezCuervo <107006627+AGomezCuervo@users.noreply.github.com>
2023-08-04 12:11:07 -0500

    fixing styles

--- client/src/components/NavBarInicio/NavBarInicio.jsx
+++ client/src/components/NavBarInicio/NavBarInicio.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/imagenes/1 (3) (2).png";
 import "./NavBarInicio.css";
