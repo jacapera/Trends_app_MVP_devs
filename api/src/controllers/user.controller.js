fa1fc5b9fb021f9a2be0a575cb1f050d4597550a
EBosch14 <enzo.jbosch@gmail.com>
2023-08-05 04:44:57 -0300

    refactor(api/): :recycle: refactor code and change input "emai" for "user" to login page

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -5,117 +5,105 @@ const { findAccount } = require("../helpers/findAccount");
 
 const getUserProfile = async (user) => {
   let foundedUser = null;
-  try {
-    if (user.type.toLowerCase() === "company")
-      foundedUser = await Company.scope(
-        "withoutId",
-        "withoutPassword"
-      ).findByPk(user.id);
-    if (user.type.toLowerCase() === "professional")
-      foundedUser = await User.scope("withoutId", "withoutPassword").findByPk(
-        user.id
-      );
-    if (user.type.toLowerCase() === "student")
-      foundedUser = await User.scope(
-        "withoutId",
-        "withoutPassword",
-        "student"
-      ).findByPk(user.id);
-    if (user.type.toLowerCase() === "admin")
-      foundedUser = await Admin.scope("withoutId", "withoutPassword").findByPk(
-        user.id
-      );
-    return foundedUser;
-  } catch (error) {
-    throw new Error("Could not find user in db.");
-  }
+  if (user.type.toLowerCase() === "company")
+    foundedUser = await Company.scope("withoutId", "withoutPassword").findByPk(
+      user.id
+    );
+  if (user.type.toLowerCase() === "professional")
+    foundedUser = await User.scope("withoutId", "withoutPassword").findByPk(
+      user.id
+    );
+  if (user.type.toLowerCase() === "student")
+    foundedUser = await User.scope(
+      "withoutId",
+      "withoutPassword",
+      "student"
+    ).findByPk(user.id);
+  if (user.type.toLowerCase() === "admin")
+    foundedUser = await Admin.scope("withoutId", "withoutPassword").findByPk(
+      user.id
+    );
+  return foundedUser;
 };
 
 const changeUserPassword = async (userId, newPassword, currentPassword) => {
-  try {
-    const foundedUser = await findAccount({ id: userId });
-    if (!foundedUser)
-      throw new Error("No valid user found to change password.");
-    if (!(await foundedUser.comparePassword(currentPassword)))
-      throw new Error(
-        "The entered password does not match the saved password."
-      );
-    if (currentPassword === newPassword)
-      throw new Error(
-        "The current password has to be different from the previous one."
-      );
-    return await foundedUser.update({ password: newPassword });
-  } catch (error) {
-    throw new Error(error.message);
-  }
+  const foundedUser = await findAccount({ id: userId });
+  if (!foundedUser) throw new Error("No valid user found to change password.");
+  if (!(await foundedUser.comparePassword(currentPassword)))
+    throw new Error("The entered password does not match the saved password.");
+  if (currentPassword === newPassword)
+    throw new Error(
+      "The current password has to be different from the previous one."
+    );
+  return await foundedUser.update({ password: newPassword });
 };
 
 const putUserProfile = async (profile, profileData) => {
-    const updatedProfile = await profile.update(profileData);
+  const updatedProfile = await profile.update(profileData);
 
-    return updatedProfile;
+  return updatedProfile;
 };
 
 const deleteUserProfile = async (id) => {
-    const deletedProfile = await User.destroy({ where: { id } });
+  const deletedProfile = await User.destroy({ where: { id } });
 
-    return deletedProfile;
+  return deletedProfile;
 };
 
 const getUserFeed = async (id, usersType) => {
-    let target;
+  let target;
 
-    // Se obtiene el usuario objetivo por su id
-    target = await getUserById(id);
+  // Se obtiene el usuario objetivo por su id
+  target = await getUserById(id);
 
-    // Si el id no corresponde a un usuario
-    // se verifica si es de un trabajo
-    if (!target) {
-      target = await getJobById(id);
-    }
-    if (!target) {
-      return { error: "Nothing found!" };
-    }
+  // Si el id no corresponde a un usuario
+  // se verifica si es de un trabajo
+  if (!target) {
+    target = await getJobById(id);
+  }
+  if (!target) {
+    return { error: "Nothing found!" };
+  }
 
-    // Se obtiene la lista de usuarios con el type especificado
-    let users;
+  // Se obtiene la lista de usuarios con el type especificado
+  let users;
 
-    users = await User.findAll({
-      where: { type: usersType },
+  users = await User.findAll({
+    where: { type: usersType },
+    attributes: {
+      exclude: [
+        "password",
+        ...(usersType === "student"
+          ? ["info_company_name", "info_position"]
+          : usersType === "professional"
+          ? ["academic_level"]
+          : []),
+      ],
+    },
+  });
+
+  if (!users.length) {
+    users = await Company.findAll({
       attributes: {
-        exclude: [
-          "password",
-          ...(usersType === "student"
-            ? ["info_company_name", "info_position"]
-            : usersType === "professional"
-            ? ["academic_level"]
-            : []),
-        ],
+        exclude: ["password"],
+      },
+      include: {
+        model: Job,
+        attributes: {
+          exclude: ["companyId"],
+        },
       },
     });
+  }
 
-    if (!users.length) {
-      users = await Company.findAll({
-        attributes: {
-          exclude: ["password"],
-        },
-        include: {
-          model: Job,
-          attributes: {
-            exclude: ["companyId"],
-          },
-        },
-      });
-    }
+  if (!users.length) {
+    return { error: "No users of the specified type were found" };
+  }
 
-    if (!users.length) {
-      return { error: "No users of the specified type were found" }
-    }
+  // Se calcula el feed utilizando el algoritmo de matcheo
+  const matches = matcher(users, target);
 
-    // Se calcula el feed utilizando el algoritmo de matcheo
-    const matches = matcher(users, target);
-
-    return matches;
+  return matches;
 };
 
 module.exports = {
