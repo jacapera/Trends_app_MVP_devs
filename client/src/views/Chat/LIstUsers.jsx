eea499298577d17730916b4c7b66367e6d68d239
JeremiasVillane <vil.jeremias@gmail.com>
2023-08-05 23:33:41 -0300

    frontend chat integration

--- client/src/views/Chat/LIstUsers.jsx
+++ client/src/views/Chat/LIstUsers.jsx
@@ -3,35 +3,18 @@ import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectAllUsersChat, setAllUsersChat } from '../../Redux/usersChatSlice';
 import { setError } from '../../Redux/chatSlice';
-import { Link } from 'react-router-dom';
+const viteUrl = import.meta.env.VITE_URL;
 
-const LIstUsers = ({ onUserSelect }) => {
+const ListUsers = ({ onUserSelect }) => {
 
   // Estados Globales
   // -------------------
   const allUsers = useSelector(selectAllUsersChat);
-  const token = useSelector(state => state.usersChat.token);
-  const user = useSelector(state => state.usersChat);
+  const user = useSelector(state => state.users.user);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    if(user.access){
-      axios.get('http://localhost:3007/api/v1/users', {
-        headers: {
-          "Authorization": `Bearer ${token}`,
-          "Content-Type": "application/json",
-        }
-      })
-        .then(({data}) => {
-          console.log('allUsers: ', data)
-          const users = data.filter(item => item.user_id !== user.user_id)
-          dispatch(setAllUsersChat(users));
-          dispatch(setError(""));
-        }).catch(error => {
-          dispatch(setError(error.response.data.response))
-        })
-    }
-  },[user.access])
+  console.log(allUsers)
+
 
   return (
     <div className='flex flex-col w-[100%] h-[auto] mt-[50px] '>
@@ -43,13 +26,11 @@ const LIstUsers = ({ onUserSelect }) => {
             className='flex h-[50px] items-center gap-[5px] cursor-pointer'
           >
             <div className='flex w-7 h-7 ml-[5px] rounded-full bg-gray-500'>
-              <Link>
-                <img className='w-full h-full object-cover rounded-full' src={`http://localhost:3007/${user.image}`} alt='imagen de perfil' />
-              </Link>
+              <img className='w-full h-full object-cover rounded-full' src={`${user.profile_image}`} alt='imagen de perfil' />
             </div>
             <h2
               className=" text-black"
-              key={user.user_id}>{user.userName}
+              key={user.user_id}>{user.username}
             </h2>
           </div>
         ))
@@ -58,4 +39,4 @@ const LIstUsers = ({ onUserSelect }) => {
   )
 }
 
-export default LIstUsers
+export default ListUsers
