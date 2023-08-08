4d76a376b8fd0483aa14312ce347a23d6965a608
JeremiasVillane <vil.jeremias@gmail.com>
2023-08-08 18:36:17 -0300

    modularization admin handlers/controllers

---
+++ api/src/controllers/admin.controllers/totalChats.js
@@ -0,0 +1,31 @@
+const { User, Chat, conn } = require("../../db");
+
+module.exports = async (userType) => {
+  //userType can be: "professional", "student", "company"
+  return await Chat.findAll({
+    attributes: [
+      [conn.fn("COUNT", conn.col("chat_id")), "total_chats"],
+      [
+        // conn.fn("TO_CHAR", conn.col("created_at"), "YYYY-MM-DD HH24:MI"),
+        conn.fn("DATE_TRUNC", "hour", conn.col("created_at")),
+        "creation_date",
+      ],
+    ],
+    include: [
+      {
+        model: User,
+        as: "UserSent",
+        where: { type: userType },
+        attributes: [],
+      },
+      {
+        model: User,
+        as: "UserReceived",
+        where: { type: "professional" },
+        attributes: [],
+      },
+    ],
+    group: [conn.fn("DATE_TRUNC", "hour", conn.col("created_at"))],
+    // group: [conn.fn("TO_CHAR", conn.col("created_at"), "YYYY-MM-DD HH24:MI")],
+  });
+};
---
+++ api/src/handlers/admin.handlers/getMessages.js
@@ -0,0 +1,17 @@
+const { totalMessages } = require("../../controllers/admin.controllers");
+
+module.exports = async (req, res) => {
+  const { userType } = req.params;
+  console.log(userType);
+  try {
+    if (
+      userType.toLowerCase() !== "student" &&
+      userType.toLowerCase() !== "professional"
+    )
+      throw new Error("Invalid params.");
+    const result = await totalMessages(userType);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
