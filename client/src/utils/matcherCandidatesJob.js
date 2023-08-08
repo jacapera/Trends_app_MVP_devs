2815b0f01d8454dcf637a7981d39c39d774891b7
GDavidDias <guillermo.david.dias@gmail.com>
2023-08-08 20:44:07 -0300

    conexion al back de feed de empresas

--- client/src/utils/matcherCandidatesJob.js
+++ client/src/utils/matcherCandidatesJob.js
@@ -1,143 +1,8 @@
 import axios from 'axios';
 
-const student =[
-    {
-        type: "student",
-        name: "Juan Perez",
-        username: "juanperez",
-        birth: "2001-10-08",
-        email: "juan.perez@example.com",
-        profile_bio:
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam ipsam officia alias sapiente blanditiis ipsa nisi odio nam maiores corporis doloremque culpa itaque aspernatur inventore hic, sint quisquam voluptatibus iusto!",
-        profile_image:
-          "https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
-        profile_city: "Buenos Aires",
-        profile_country: "Argentina",
-        profile_support: true,
-        info_career: ["Desarrollo de Software"],        
-      },
-      {
-        type: "student",
-        name: "Maria Sanchez",
-        username: "mariasanchez",
-        birth: "2001-10-08",
-        email: "maria.sanchez@example.com",
-        profile_bio:
-            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam ipsam officia alias sapiente blanditiis ipsa nisi odio nam maiores corporis doloremque culpa itaque aspernatur inventore hic, sint quisquam voluptatibus iusto!",
-        profile_image:
-            "https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
-        profile_city: "Madrid",
-        profile_country: "España",
-        profile_support: false,
-        info_career: ["Recursos Humanos"],
-      },
-      {
-        type: "student",
-        name: "John Smith",
-        username: "johnsmith",
-        birth: "2001-10-08",
-        email: "john.smith@example.com",
-        profile_bio:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam ipsam officia alias sapiente blanditiis ipsa nisi odio nam maiores corporis doloremque culpa itaque aspernatur inventore hic, sint quisquam voluptatibus iusto!",
-        profile_image:
-        "https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
-        profile_city: "New York",
-        profile_country: "United States",
-        profile_support: true,
-        info_career: ["Psicoterapia"],
-      },
-      {
-        type: "student",
-        name: "Laura Johnson",
-        username: "laurajohnson",
-        birth: "2001-10-08",
-        email: "laura.johnson@example.com",
-        profile_bio:
-            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam ipsam officia alias sapiente blanditiis ipsa nisi odio nam maiores corporis doloremque culpa itaque aspernatur inventore hic, sint quisquam voluptatibus iusto!",
-        profile_image:
-            "https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
-        profile_city: "London",
-        profile_country: "United Kingdom",
-        profile_support: false,
-        info_career: ["Abogacía"],
-      },
-      {
-        type: "student",
-        name: "Carlos Fernández",
-        username: "carlosfernandez",
-        birth: "2001-10-08",
-        email: "carlos.fernandez@example.com",
-        profile_bio:
-            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam ipsam officia alias sapiente blanditiis ipsa nisi odio nam maiores corporis doloremque culpa itaque aspernatur inventore hic, sint quisquam voluptatibus iusto!",
-        profile_image:
-            "https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
-        profile_city: "Mexico City",
-        profile_country: "Mexico",
-        profile_support: true,
-        info_career: ["Abogacía"],
-      },
-      {
-        type: "professional",
-        name: "Ana Rodriguez",
-        username: "anarodriguez",
-        birth: "2001-10-08",
-        email: "ana.rodriguez@example.com",
-        profile_bio:
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam ipsam officia alias sapiente blanditiis ipsa nisi odio nam maiores corporis doloremque culpa itaque aspernatur inventore hic, sint quisquam voluptatibus iusto!",
-        profile_image:
-          "https://images.unsplash.com/photo-1596854407944-bf87f6fdd49e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
-        profile_city: "Madrid",
-        profile_country: "España",
-        profile_support: true,
-        info_career: [
-            "Desarrollo e implementación de estrategias de marketing",
-            "Gestión de equipos y proyectos",
-            "Incremento de la visibilidad de la marca",
-          ]
-      },
-      {
-        type: "professional",
-        name: "Michael Brown",
-        username: "michaelbrown",
-        birth: "2001-10-08",
-        email: "michael.brown@example.com",
-        profile_bio:
-            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam ipsam officia alias sapiente blanditiis ipsa nisi odio nam maiores corporis doloremque culpa itaque aspernatur inventore hic, sint quisquam voluptatibus iusto!",
-        profile_image:
-            "https://images.unsplash.com/photo-1596854407944-bf87f6fdd49e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
-        profile_city: "New York",
-        profile_country: "United States",
-        profile_support: false,
-        info_career: [
-            "Desarrollo de aplicaciones web y móviles",
-            "Colaboración en equipos de desarrollo ágiles",
-            "Optimización de rendimiento de software",
-          ],
-      },
-      {
-        type: "professional",
-        name: "Sophie Martin",
-        username: "sophiemartin",
-        birth: "2001-10-08",
-        email: "sophie.martin@example.com",
-        profile_bio:
-            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam ipsam officia alias sapiente blanditiis ipsa nisi odio nam maiores corporis doloremque culpa itaque aspernatur inventore hic, sint quisquam voluptatibus iusto!",
-        profile_image:
-            "https://images.unsplash.com/photo-1596854407944-bf87f6fdd49e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
-        profile_city: "Paris",
-        profile_country: "France",
-        profile_support: true,
-        info_career: [
-            "Evaluación y selección de personal",
-            "Desarrollo de planes de capacitación",
-            "Gestión del clima laboral",
-          ],
-      }
-];
-
 export const matcherCandidatesJob = async(dataJob) => {
     console.log("que trae dataJobs: ", dataJob)
-    //?SOLO PARA USO DE PRUEBAS, CUANDO LAS RUTAS ESTEN SE GUARDAN
+    
     let candidates = [];
 
     //!Armo las condiciones para consultar los estudiantes con campos clave
@@ -145,20 +10,21 @@ export const matcherCandidatesJob = async(dataJob) => {
     // >>/search/users?type=student&info_skills=Desarrollo web,Bases de datos    
     if(dataJob.levelRequired) strStudentHigh += '&academic_level='+dataJob.levelRequired;
     //if(dataJob.studyArea) strStudentHigh+='&academic_area='+dataJob.studyArea;
-    //if(dataJob.benefits) strStudentHigh+='&info_interests='+dataJob.benefits.join(',');
     //if(dataJob.skillsRequired) strStudentHigh+='&info_skills='+dataJob.skillsRequired.join(',');
     //if(dataJob.jobGoal) strStudentHigh+='&info_goals='+dataJob.jobGoal.join(',');
+    //if(dataJob.benefits) strStudentHigh+='&info_interests='+dataJob.benefits.join(',');
     //console.log("que tiene level_required: ", dataJob.academic.level_required);
     
-    let query = `http://localhost:3001/api/v1/search/users?type=student`;
-    query += strStudentHigh;
+    let querySt = `http://localhost:3001/api/v1/search/users?type=student`;
+    querySt += strStudentHigh;
 
-    console.log("como arma query: ", query);
+    console.log("como arma query de Estudiantes: ", querySt);
 
       try{
-        const response = (await axios.get(query)).data;
-        console.log("que trae response <matcherCandidatesJob>: ", response);
-
+        //?TRAIGO DATOS DE CANDIDATOS ESTUDIANTES
+        const response = (await axios.get(querySt,{withCredentials: "include"})).data.data;
+        console.log("que trae response estudiantes <matcherCandidatesJob>: ", response);
+        
         response
           ?candidates = [...response]
           :candidates =[]
@@ -166,8 +32,34 @@ export const matcherCandidatesJob = async(dataJob) => {
       }catch(error){
         console.log("error al traer datos de estudiantes <matcherCandidates>: ", error.message);
       }
-    
 
+    //!Armo las condiciones para consultar los PROFESIONALES con campos clave
+    let strProfesionalHigh='';
+    // >>/search/users?type=professional&info_skills=Desarrollo web,Bases de datos    
+    //if(dataJob.levelRequired) strProfesionalHigh += '&academic_formation='+dataJob.levelRequired;
+    //if(dataJob.studyArea) strProfesionalHigh+='&academic_area='+dataJob.studyArea;
+    //if(dataJob.benefits) strProfesionalHigh+='&info_interests='+dataJob.benefits.join(',');
+    //if(dataJob.skillsRequired) strProfesionalHigh+='&info_skills='+dataJob.skillsRequired.join(',');
+    //if(dataJob.jobGoal) strProfesionalHigh+='&info_goals='+dataJob.jobGoal.join(',');
+    //console.log("que tiene level_required: ", dataJob.academic.level_required);
+    
+    let queryPr = `http://localhost:3001/api/v1/search/users?type=professional`;
+    queryPr += strProfesionalHigh;
+
+    console.log("como arma query de Profesionales: ", queryPr);
+
+    try{
+      //?TRAIGO DATOS DE CANDIDATOS PROFESIONALES
+      const responsePr = (await axios.get(queryPr,{withCredentials: "include"})).data.data;
+      console.log("que trae response profesionales <matcherCandidatesJob>: ", responsePr);
+      
+      responsePr
+        ?candidates = candidates.concat(responsePr)
+        :candidates =[...candidates]
+
+    }catch(error){
+      console.log("error al traer datos de profesionales <matcherCandidates>: ", error.message);
+    }
 
 
     //!Armo las condiciones para consultar estudiantes con campos de media
