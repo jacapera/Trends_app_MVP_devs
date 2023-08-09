764d5b08d3d7bfcba3e39a254c107faab79e5f36
EBosch14 <enzo.jbosch@gmail.com>
2023-08-09 10:14:28 -0300

    perf(api/): :zap: change the way data is sent to the frontend

--- api/src/controllers/admin.controllers.js
+++ api/src/controllers/admin.controllers.js
@@ -1,9 +1,53 @@
 const { Company, User, Admin, Chat, Message, conn } = require("../db");
+const DATE_BY_MIN = "YYYY-MM-DD HH24:MI TZ";
 
+// const findAllUsers = async () => {
+//   const users = await User.scope("withoutPassword").findAll();
+//   const companies = await Company.scope("withoutPassword").findAll();
+//   return [...users, ...companies];
+// };
 const findAllUsers = async () => {
-  const users = await User.scope("withoutPassword").findAll();
-  const companies = await Company.scope("withoutPassword").findAll();
-  return [...users, ...companies];
+  const studentsData = await User.findAll({
+    attributes: [
+      [conn.fn("COUNT", conn.col("id")), "total"],
+      [
+        conn.fn("TO_CHAR", conn.col("createdAt"), DATE_BY_MIN),
+        "creation_date_utc",
+      ],
+    ],
+    where: { type: "student" },
+    group: [conn.fn("TO_CHAR", conn.col("createdAt"), DATE_BY_MIN)],
+    order: [[conn.fn("TO_CHAR", conn.col("createdAt"), DATE_BY_MIN), "ASC"]],
+  });
+  const professionalsData = await User.findAll({
+    attributes: [
+      [conn.fn("COUNT", conn.col("id")), "total"],
+      [
+        conn.fn("TO_CHAR", conn.col("createdAt"), DATE_BY_MIN),
+        "creation_date_utc",
+      ],
+    ],
+    where: { type: "professional" },
+    group: [conn.fn("TO_CHAR", conn.col("createdAt"), DATE_BY_MIN)],
+    order: [[conn.fn("TO_CHAR", conn.col("createdAt"), DATE_BY_MIN), "ASC"]],
+  });
+
+  const companiesData = await Company.findAll({
+    attributes: [
+      [conn.fn("COUNT", conn.col("id")), "total"],
+      [
+        conn.fn("TO_CHAR", conn.col("createdAt"), DATE_BY_MIN),
+        "creation_date_utc",
+      ],
+    ],
+    group: [conn.fn("TO_CHAR", conn.col("createdAt"), DATE_BY_MIN)],
+    order: [[conn.fn("TO_CHAR", conn.col("createdAt"), DATE_BY_MIN), "ASC"]],
+  });
+  return {
+    students: studentsData,
+    professionals: professionalsData,
+    companies: companiesData,
+  };
 };
 
 const createNewAdmin = async (userData) => {
