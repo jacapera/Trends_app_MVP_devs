5d2309134731299e27839abf8e9e307ac7e53001
JeremiasVillane <vil.jeremias@gmail.com>
2023-08-08 22:52:26 -0300

    bugfix: company image deletion, change: profile image upload response

--- api/src/controllers/image.controllers/postProfileImage.js
+++ api/src/controllers/image.controllers/postProfileImage.js
@@ -39,5 +39,5 @@ module.exports = async (userId, userType, filename, path) => {
     return { error: error.message };
   }
 
-  return { message: "Profile image updated" };
+  return { imageId: savedImage.id, imagePath: path };
 };
