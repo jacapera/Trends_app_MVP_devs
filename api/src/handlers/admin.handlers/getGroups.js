263f81854eedcc34f58f47585ee0bbcbaa364679
EBosch14 <enzo.jbosch@gmail.com>
2023-08-10 12:15:13 -0300

    feat(api/): :sparkles: add feature to find all groups

--- api/src/handlers/admin.handlers/getGroups.js
+++ api/src/handlers/admin.handlers/getGroups.js
@@ -1,10 +1,16 @@
+const totalGroups = require("../../controllers/admin.controllers/totalGroups");
 const { getAllGroups } = require("../../controllers/chatroom.controllers");
 
 module.exports = async (req, res) => {
+  const { userType } = req.params;
   try {
-    const allGroups = await getAllGroups(null, "admin");
-
-    res.status(200).json(allGroups);
+    if (
+      userType.toLowerCase() !== "student" &&
+      userType.toLowerCase() !== "professional"
+    )
+      throw new Error("Invalid params.");
+    const result = await totalGroups(userType);
+    res.status(200).json(result);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
